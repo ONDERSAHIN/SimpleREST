baa39205069eb910b53fffa83c00a4fd2ee432d8
onder_sh <oondersahin@gmail.com>
2017-07-21 16:39:39 +0300

    change message 2

--- src/main/java/com/h2h/Driver.java
+++ src/main/java/com/h2h/Driver.java
@@ -18,7 +18,7 @@ public class Driver extends HttpServlet{
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         Gson myGson = new Gson();
         MyData md = new MyData();
-        md.setTitle("HELLLO LIFE 2 :) !");
+        md.setTitle("HELLLO LIFE 3 :) !");
         resp.getWriter().write(myGson.toJson(md));
     }
 
